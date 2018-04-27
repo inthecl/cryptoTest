54d041002ac368013c5c198f3dadce70f69051c6
hjkim <dudnwjs@gmail.com>
2018-04-27 10:30:25 +0900

    add pagenation

--- src/Market.js
+++ src/Market.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import _ from 'underscore';
+import imgTestPet from './img_test_pet.png';
 
 class Market extends Component {
   constructor(props) {
@@ -8,53 +9,62 @@ class Market extends Component {
     this.exampleItems = _.range(1, 151).map(i => ({ id: i, name: `Item ${i}` }))
   }
 
+
+  drawPagenation(pagenum) {
+    return
+    (
+      <ul className="pagination">
+        <li className="disabled"><Link to={"/Market/"+String(pagenum)}><i className="material-icons">chevron_left</i></Link></li>
+        <li className="active"><Link to={"/Market/"+String(pagenum)}>{pagenum}</Link></li>
+        <li className="waves-effect"><Link to={"/Market/"+String(parseInt(pagenum)+1)}>{parseInt(pagenum)+1}</Link></li>
+        <li className="waves-effect"><Link to={"/Market/"+String(parseInt(pagenum)+2)}>{parseInt(pagenum)+2}</Link></li>
+        <li className="waves-effect"><Link to={"/Market/"+String(parseInt(pagenum)+3)}>{parseInt(pagenum)+3}</Link></li>
+        <li className="waves-effect"><Link to={"/Market/"+String(parseInt(pagenum)+4)}>{parseInt(pagenum)+4}</Link></li>
+        <li className="waves-effect"><Link to={"/Market/"+String(parseInt(pagenum)+5)}><i className="material-icons">chevron_right</i></Link></li>
+      </ul>
+    )
+  }
+
   render() {
-    const { pageNum } = this.props;
-    console.log(pageNum)
-    console.log(this.exampleItems)
+    const { pagenum } = this.props.match.params;
+    const lastItem = this.exampleItems.length
+    const lastPage = lastItem/12;
+    console.log(lastPage)
+    const startItem = (pagenum-1)*12
+    let endItem = pagenum*12
+
+    if(endItem > lastItem) endItem = lastItem
+    const pages = this.exampleItems.slice(startItem, endItem)
 
     return (
       <div className='center'>
         <div className="row">
-          <div className="col s12 m4">
-            <div className="card">
-              <div className="card-image waves-effect waves-block waves-light">
-                <img src="https://www.google.co.kr/logos/doodles/2018/earth-day-2018-6526947692642304-s.png"></img>
+          {pages.map(item =>
+            <div key={item.id}>
+              <div className="col s12 m6 l3">
+                <div className="card">
+                  <div className="card-image">
+                    <img src={imgTestPet}/>
+                    <span className="card-title">Card Title</span>
+                  </div>
+                  <div className="card-content">
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                  I am convenient because I require little markup to use effectively.</p>
+                  </div>
+                  <div className="card-action">
+                    <Link to="/Market/2">{item.name}</Link>
+                  </div>
+                </div>
               </div>
-              <div className="card-content">
-                <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
-              </div>
-              <div className="card-reveal">
-                <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
-                <p>Here is some more information about this product that is only revealed once clicked on.</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="col s12 m4">
-            <div className="card">
-              <div className="card-image waves-effect waves-block waves-light">
-                <img src="https://www.google.co.kr/logos/doodles/2018/earth-day-2018-6526947692642304-s.png"></img>
-              </div>
-              <div className="card-content">
-                <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
-              </div>
-              <div className="card-reveal">
-                <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
-                <p>Here is some more information about this product that is only revealed once clicked on.</p>
-              </div>
-            </div>
-          </div>
+            </div>)}
         </div>
 
         <ul className="pagination">
-          <li className="disabled"><Link to="/Market/0"><i className="material-icons">chevron_left</i></Link></li>
-          <li className="active"><Link to="/Market/1">1</Link></li>
-          <li className="waves-effect"><Link to="/Market/2">2</Link></li>
-          <li className="waves-effect"><Link to="/Market/3">3</Link></li>
-          <li className="waves-effect"><Link to="/Market/4">4</Link></li>
-          <li className="waves-effect"><Link to="/Market/5">5</Link></li>
-          <li className="waves-effect"><Link to="/Market/6"><i className="material-icons">chevron_right</i></Link></li>
+          <li className="disabled"><Link to={"/Market/"+String(pagenum)}><i className="material-icons">chevron_left</i></Link></li>
+          {[1,2,3,4,5].map(i =>
+              (parseInt(pagenum) + i - 1)   < lastPage ? <li className="waves-effect"><Link to={"/Market/"+String(parseInt(pagenum)+i)}>{parseInt(pagenum)+i}</Link></li> : <li/>
+          )}
+          <li className="waves-effect"><Link to={"/Market/"+String(parseInt(pagenum)+5)}><i className="material-icons">chevron_right</i></Link></li>
         </ul>
       </div>
     )
