7273c311ab4bba296b56bf48e141473fa2b8dffa
kimjaeyoung <zangon88@gmail.com>
2018-04-28 12:50:27 +0900

    complete market pagenation

--- src/Market.js
+++ src/Market.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import _ from 'underscore';
 import imgTestPet from './img_test_pet.png';
 
@@ -9,20 +9,45 @@ class Market extends Component {
     this.exampleItems = _.range(1, 151).map(i => ({ id: i, name: `Item ${i}` }))
   }
 
-
-  drawPagenation(pagenum) {
-    return
-    (
-      <ul className="pagination">
-        <li className="disabled"><Link to={"/Market/"+String(pagenum)}><i className="material-icons">chevron_left</i></Link></li>
-        <li className="active"><Link to={"/Market/"+String(pagenum)}>{pagenum}</Link></li>
-        <li className="waves-effect"><Link to={"/Market/"+String(parseInt(pagenum)+1)}>{parseInt(pagenum)+1}</Link></li>
-        <li className="waves-effect"><Link to={"/Market/"+String(parseInt(pagenum)+2)}>{parseInt(pagenum)+2}</Link></li>
-        <li className="waves-effect"><Link to={"/Market/"+String(parseInt(pagenum)+3)}>{parseInt(pagenum)+3}</Link></li>
-        <li className="waves-effect"><Link to={"/Market/"+String(parseInt(pagenum)+4)}>{parseInt(pagenum)+4}</Link></li>
-        <li className="waves-effect"><Link to={"/Market/"+String(parseInt(pagenum)+5)}><i className="material-icons">chevron_right</i></Link></li>
-      </ul>
-    )
+  drawPagenation(pagenum, lastPage) {
+    let targetPages = []
+    for(let i = -1; i < 6; i++) {
+      targetPages.push(parseInt(pagenum) + i)
+    }
+    targetPages = targetPages.filter((num) => num < lastPage+2)
+    console.log(targetPages)
+    let first = targetPages[0]
+    let len = 7 - targetPages.length;
+    for(let i = 0; i < len; i++) {
+      targetPages.unshift(first-(i+1))
+    }
+    console.log(targetPages)
+    let idx = 0;
+    let activePage = 0;
+    const listItems = targetPages.map((num) => {
+        idx = idx + 1
+        if(num == 0) {
+          return <li className="disabled"><Link to={"/Market/"+String(num)}><i className="material-icons">chevron_left</i></Link></li>
+        }
+        if(idx == 1) {
+          return <li className="waves-effect"><Link to={"/Market/"+String(parseInt(pagenum)-1)}><i className="material-icons">chevron_left</i></Link></li>
+        }
+        if(pagenum == num) {
+          activePage = num;
+          return <li className="active"><Link to={"/Market/"+String(num)}>{num}</Link></li>
+        }
+        if(activePage + 1 > lastPage+1 && idx == targetPages.length) {
+          return <li className="disabled"><Link to={"/Market/"+String(activePage)}><i className="material-icons">chevron_right</i></Link></li>
+        }
+        if(idx == targetPages.length) {
+          return <li className="waves-effect"><Link to={"/Market/"+String(parseInt(activePage)+1)}><i className="material-icons">chevron_right</i></Link></li>
+        }
+        return <li className="waves-effect"><Link to={"/Market/"+String(num)}>{num}</Link></li>
+      } 
+    );
+    return (
+      <ul class="pagination">{listItems}</ul>
+    );
   }
 
   render() {
@@ -32,6 +57,8 @@ class Market extends Component {
     console.log(lastPage)
     const startItem = (pagenum-1)*12
     let endItem = pagenum*12
+    if(pagenum < 1) return <Redirect to="/Market/1"/>
+    if(pagenum > lastPage + 1) return <Redirect to="/Market/1"/>
 
     if(endItem > lastItem) endItem = lastItem
     const pages = this.exampleItems.slice(startItem, endItem)
@@ -59,13 +86,7 @@ class Market extends Component {
             </div>)}
         </div>
 
-        <ul className="pagination">
-          <li className="disabled"><Link to={"/Market/"+String(pagenum)}><i className="material-icons">chevron_left</i></Link></li>
-          {[1,2,3,4,5].map(i =>
-              (parseInt(pagenum) + i - 1)   < lastPage ? <li className="waves-effect"><Link to={"/Market/"+String(parseInt(pagenum)+i)}>{parseInt(pagenum)+i}</Link></li> : <li/>
-          )}
-          <li className="waves-effect"><Link to={"/Market/"+String(parseInt(pagenum)+5)}><i className="material-icons">chevron_right</i></Link></li>
-        </ul>
+        {this.drawPagenation(pagenum,lastPage)}
       </div>
     )
   }
